9397df2cfbf4382a68252757c40048ab6735292b
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-09-13 15:17:42 -0400

    test

--- WilliamRobot/src/robot/Robot.java
+++ WilliamRobot/src/robot/Robot.java
@@ -1,5 +1,33 @@
-package robot;
+package duel;
 
-public class Robot {
+import duel.CharacterA;
+import duel.CharacterB;
+import duel.Duel;
+import duel.Dueler;
+
+public class Duel {
+
+	
+
+	private Dueler d1;
+	private Dueler d2;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
+	
 
 }
