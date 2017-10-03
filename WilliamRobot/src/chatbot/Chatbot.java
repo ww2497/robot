45f7f2c018644f35fd570c8994fd893c30a1dd1a
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2017-10-03 15:10:39 -0400

    Work on things at home, All isTriggered set up for each topic.

--- WilliamRobot/src/chatbot/Chatbot.java
+++ WilliamRobot/src/chatbot/Chatbot.java
@@ -8,10 +8,16 @@ public class Chatbot {
 	private Topic mark;
 	private Topic theo;
 	private Topic devin;
+	private String location;
+	private String month;
 	
 
 	public Chatbot() {
 		william = new ChatbotWilliam();
+		mark = new ChatbotMark();
+		theo = new ChatbotTheo();
+		devin = new ChatbotDevin();
+		location = "Unknown";
 		username = "Unknown User";
 		chatting = true;
 	}
@@ -19,19 +25,33 @@ public class Chatbot {
 	public String getUsername() {
 		return username;
 	}
+	public String getLocation() {
+		return location;
+	}
 	
 	public void startChatting() {
-		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your inputs. Tell me your name.");
+		ChatbotMain.print("Hi! I am your New York City Tour Bot, please tell me your name?");
 		username = ChatbotMain.getInput();
+		ChatbotMain.print("What borough are you in right now?");
+		location = ChatbotMain.getInput();
+		
 		while(chatting) {
 			ChatbotMain.print("What would you like to talk about?");
 			String response = ChatbotMain.getInput();
 			if(william.isTriggered(response)) {
 				chatting = false;//exits the while loop. IMPORTANT you get graded for this!
 				william.talk(response);
-				
+			}else if(mark.isTriggered(response)){
+				chatting = false;
+				mark.talk(response);
+			}else if(theo.isTriggered(response)) {
+				chatting = false;
+				theo.talk(response);
+			}else if(devin.isTriggered(response)) {
+				chatting = false;
+				devin.talk(response);
 			}else {
-				ChatbotMain.print("I'm sorry. I don't understand. I never said I was perfect.");
+				ChatbotMain.print("I don't know anything about that. What else do you want to talk about?");
 			}
 		}
 	}
--- WilliamRobot/src/chatbot/ChatbotMark.java
+++ WilliamRobot/src/chatbot/ChatbotMark.java
@@ -8,24 +8,27 @@ public class ChatbotMark implements Topic {
 	private String response;
 	
 	public ChatbotMark() {
-		String[] temp = {"stuff","things","something","nothing"};
+		String[] temp = {"Brooklyn","Manhatton","Queens","Bronx","Staten Island"};
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "bloop";
+		secretKeyword = "New Amsterdam";
 		response = "";
 	}
 
 	@Override
 	public void talk(String response) {
-		ChatbotMain.print("Hey! We in Brooklyn, NYC and I know all the best spots all over the city. You want duh best pizza spot, pssh, fuhgettaboutit. Name spot you wanna know about.");
+		ChatbotMain.print("Hey! We in Brooklyn, NYC and I know all the best spots all over the city. You want duh best pizza spot, pssh, fuhgettaboutit. Where are you right now?");
 		response = ChatbotMain.getInput();
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0 ) == -1) {
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0 ) >= 0) {
 				ChatbotMain.print("Bleep bloop, How did you know i was a robot?!");
 				response = ChatbotMain.getInput();
 			}else {
-				ChatbotMain.print("Yeah, thats cool I guess, what else you got?");
-				response = ChatbotMain.getInput();
+				for(int i = 0; i < keywords.length; i++) {
+					if(ChatbotMain.findKeyword(response, keywords[i], 0) >= 0) {
+						
+					}
+				}
 			}
 		}
 		//access stuff from other class
