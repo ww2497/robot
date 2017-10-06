ea821388c55ee41c431621ae32a8878ae8c48ac1
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2017-10-06 15:35:15 -0400

    edits 10/6/17

--- WilliamRobot/src/chatbot/ChatbotDevin.java
+++ WilliamRobot/src/chatbot/ChatbotDevin.java
@@ -13,10 +13,10 @@ public class ChatbotDevin implements Topic {
 	//
 	
 	public ChatbotDevin() {
-		String[] temp = {"sightseeing","sightsee","museums","empire state building","statue of liberty"};
+		String[] temp = {"dates","time","events"};
 		keywords = temp;
 		goodbyeKeyword = "bye";
-		secretKeyword = "date";
+		secretKeyword = "";
 		facts = "facts";
 	}
 	public String getDestinations() {
@@ -29,25 +29,15 @@ public class ChatbotDevin implements Topic {
 	}
 	@Override
 	public void talk(String response) {
-		ChatbotMain.print(" ");
+		ChatbotMain.print("Hello! I am the NYC event robot! Feel free to ask me any questions! ");
 		response = ChatbotMain.getInput();
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("Oh, you want to go to the statue of liberty?");
-				//if yes
-				ChatbotMain.print("Type in 'facts' if you want to know more about the Statue of Liberty. Type in 'directions' if you need directions to the Statue of Liberty.");
-				response = ChatbotMain.getInput();
-				ChatbotMain.chatbot.checkTriggered(response);
-				
-				ChatbotMain.chatbot.setDestination("statue of liberty");
-				response = ChatbotMain.getInput();
-			}
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
 				ChatbotMain.print(" ");
 				response = ChatbotMain.getInput();
 				
 			}else {
-				ChatbotMain.print("Yeah. That's pretty cool. However, there are things I like even more. Tell me something else.");
+				ChatbotMain.print("");
 				response = ChatbotMain.getInput();
 			}
 		}
@@ -63,7 +53,7 @@ public class ChatbotDevin implements Topic {
 			if(ChatbotMain.findKeyword(response, keywords[i],0) >= 0) {
 				return true;
 			}
-			return false;
+			
 		}
 		return false;
 	}
