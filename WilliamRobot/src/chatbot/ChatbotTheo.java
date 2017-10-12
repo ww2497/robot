e1900fca1812230b1c8713b0e3d29c08642b7959
wwu5040 <31863372+wwu5040@users.noreply.github.com>
2017-10-12 04:23:36 -0400

    Update ChatbotTheo.java

--- WilliamRobot/src/chatbot/ChatbotTheo.java
+++ WilliamRobot/src/chatbot/ChatbotTheo.java
@@ -4,7 +4,6 @@ public class ChatbotTheo implements Topic {
 	private String [] keywords;
 	private String goodbyeKeyword;
 	private String[] yes;
-	private String[] neighborhoods;
 	private String lesRec;
 	private String uesRec;
 	private String cRec;
@@ -18,18 +17,14 @@ public class ChatbotTheo implements Topic {
 	private String ciRec;
 	private String shRec;
 	private String hRec;
-	private String pizza;
 	private boolean rec;
 	
 	public ChatbotTheo() {
 		String[] temp = {"food","eat","hungry","restaurants"};
 		keywords = temp;
-		String[] localTemp = {"lower east side","upper east side","chinatown","flatbush","little italy","east village","park slope","west village","bedford","jamaica","coney island","soho","harlem"};
-		neighborhoods = localTemp;
 		goodbyeKeyword = "bye";
-		String[] asdf = {"yea","yes","ok","sure","ye","y","yeah"};
+		String[] asdf = {"yea","yes","ok","sure","ye","y","yeah","okay","k"};
 		yes = asdf;
-		pizza="pizza";
 		lesRec = "Balvanera";
 		uesRec = "Up Thai";
 		cRec = "Joe's Shanghai's";
@@ -48,134 +43,128 @@ public class ChatbotTheo implements Topic {
 //
 	@Override
 	public void talk(String response) {
+		rec=false;
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0)==-1) {
-			rec=false;
 			while(rec==false) {
-				while(ChatbotMain.chatbot.getLocation().equals("unknown")) {
-					ChatbotMain.print("I don't know where you are. Please tell me what neighborhood you're in now.");
-					response = ChatbotMain.getInput();
-					for(int i = 0; i < neighborhoods.length; i++) {
-						if(ChatbotMain.findKeyword(response, neighborhoods[i], 0) >= 0) {
-							ChatbotMain.chatbot.setLocation(neighborhoods[i]);
-							i = neighborhoods.length;
-						}
-					}
-					if(ChatbotMain.chatbot.getLocation().equals("unknown")) {
-						ChatbotMain.print("Make sure you typed the name of the WELL KNOWN neighborhood correctly!");
-					}
-				}
 				ChatbotMain.print("Would you like me to recomend somewhere to eat?");
-				response= ChatbotMain.getInput();
+				response = ChatbotMain.getInput();
 				for(int i=0;i<yes.length;i++) {
 					if(ChatbotMain.findKeyword(response,yes[i] , 0)>=0) {
+						if(ChatbotMain.chatbot.getLocation().equals("unknown")) {
+							ChatbotMain.print("I'll need to know where you are first. Please tell me what neighborhood you're currently in.");
+							response = ChatbotMain.getInput();
+							while (ChatbotMain.chatbot.getLocation() == "unknown") {
+								if(response.equals("lower east side") || response.equals("upper east side") || response.equals("chinatown") || response.equals("flatbush") || response.equals("little italy") || response.equals("east village") || response.equals("park slope") || response.equals("west village") || response.equals("bedford") || response.equals("jamaica") || response.equals("coney island") || response.equals("soho") || response.equals("harlem"))
+								{
+									ChatbotMain.chatbot.setLocation(response);
+								}
+								else
+								{
+									ChatbotMain.print("Sorry, that's not a valid neighborhood.");
+									response = ChatbotMain.getInput();
+								}
+							}
+						}
 						if(ChatbotMain.chatbot.getLocation().equals("lower east side")) {
 							ChatbotMain.print("You should go to "+lesRec+".");
 							ChatbotMain.chatbot.setDestination(lesRec);
 							ChatbotMain.chatbot.setTrain("F");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("upper east side")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("upper east side")) {
 							ChatbotMain.print("You should go to "+uesRec+".");
 							ChatbotMain.chatbot.setDestination(uesRec);
 							ChatbotMain.chatbot.setTrain("Q");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("chinatown")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("chinatown")) {
 							ChatbotMain.print("You should go to "+cRec+".");
 							ChatbotMain.chatbot.setDestination(cRec);
 							ChatbotMain.chatbot.setTrain("R");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("flatbush")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("flatbush")) {
 							ChatbotMain.print("You should go to "+fRec+".");
 							ChatbotMain.chatbot.setDestination(fRec);
 							ChatbotMain.chatbot.setTrain("2");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("little italy")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("little italy")) {
 							ChatbotMain.print("You should go to "+liRec+".");
 							ChatbotMain.chatbot.setDestination(liRec);
 							ChatbotMain.chatbot.setTrain("R");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("east village")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("east village")) {
 							ChatbotMain.print("You should go to "+evRec+".");
 							ChatbotMain.chatbot.setDestination(evRec);
 							ChatbotMain.chatbot.setTrain("F");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("park slope")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("park slope")) {
 							ChatbotMain.print("You should go to "+psRec+".");
 							ChatbotMain.chatbot.setDestination(psRec);
 							ChatbotMain.chatbot.setTrain("R");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("west village")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("west village")) {
 							ChatbotMain.print("You should go to "+wvRec+".");
 							ChatbotMain.chatbot.setDestination(wvRec);
 							ChatbotMain.chatbot.setTrain("1");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("bedford")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("bedford")) {
 							ChatbotMain.print("You should go to "+bedRec+".");
 							ChatbotMain.chatbot.setDestination(bedRec);
 							ChatbotMain.chatbot.setTrain("L");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("jamaica")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("jamaica")) {
 							ChatbotMain.print("You should go to "+jRec+".");
 							ChatbotMain.chatbot.setDestination(jRec);
 							ChatbotMain.chatbot.setTrain("L");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("coney island")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("coney island")) {
 							ChatbotMain.print("You should go to "+ciRec+".");
 							ChatbotMain.chatbot.setDestination(ciRec);
 							ChatbotMain.chatbot.setTrain("Q");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("soho")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("soho")) {
 							ChatbotMain.print("You should go to "+shRec+".");
 							ChatbotMain.chatbot.setDestination(shRec);
 							ChatbotMain.chatbot.setTrain("R");
 							rec = true;
-							i = yes.length;
 						}
-						if(ChatbotMain.chatbot.getLocation().equals("harlem")) {
+						else if(ChatbotMain.chatbot.getLocation().equals("harlem")) {
 							ChatbotMain.print("You should go to "+hRec+".");
 							ChatbotMain.chatbot.setDestination(hRec);
 							ChatbotMain.chatbot.setTrain("A");
 							rec = true;
-							i = yes.length;
 						}
 					}
 				}
-				if(rec==false){
-					ChatbotMain.print("ok I will not recomend somewhere for you to eat");
+				if (rec == false) {
+					ChatbotMain.print("Okay, I will not recomend somewhere for you to eat.");
+					rec = true;
+					ChatbotMain.chatbot.startChatting();
 				}
-				rec = true;
-			}
-			while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0)!=-1) {
-				if(ChatbotMain.findKeyword(response,pizza, 0)>=0) {
-					ChatbotMain.print("New York is well known for it pizza that is usualy made with hand tossed crust and sold in slices.");
-				}
-				else {
-				ChatbotMain.print("If that is all please say 'bye'.");
+				else
+				{
+					ChatbotMain.print("Do you need directions?");
 				}
 			}
+			
+			response = ChatbotMain.getInput();
+			for(int i=0;i<yes.length;i++) {
+				if(ChatbotMain.findKeyword(response,yes[i] , 0)>=0) {
+					ChatbotMain.chatbot.startWilliam(response);
+				}
+			}
+			ChatbotMain.print("Alright, please say 'bye' to be redirected to the main tour guide.");
 		}
-		ChatbotMain.print("Well, it was nice talking to you "+ChatbotMain.chatbot.getUsername()+"!");
+		ChatbotMain.print("Well, it was nice talking to you, "+ChatbotMain.chatbot.getUsername()+".");
 		ChatbotMain.chatbot.startChatting();
 	}
 	public boolean isTriggered(String response) {
