dd085fd3126b184045d3533d4d37f1558ad835f5
yingz <yingz@LAPTOP-APEKNB6S.fios-router.home>
2017-10-12 02:41:33 -0400

    im sorry

--- WilliamRobot/src/chatbot/ChatbotDevin.java
+++ WilliamRobot/src/chatbot/ChatbotDevin.java
@@ -1,7 +1,6 @@
 package chatbot;
 
 import java.util.Date;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 public class ChatbotDevin implements Topic {
@@ -9,7 +8,10 @@ public class ChatbotDevin implements Topic {
 	private String[] keywords;
 	private String goodbyeKeyword;
 	private String secretKeyword;
-	private String facts;
+	private String[] facts;
+	private String date1;
+	private String date2;
+	private boolean a;
 	
 	
 	//
@@ -19,7 +21,11 @@ public class ChatbotDevin implements Topic {
 		keywords = temp;
 		goodbyeKeyword = "bye";
 		secretKeyword = "time";
-		facts = "facts";
+		String[] knowledge = {"general","area","recommend"};
+		facts = knowledge;
+		date1 = "10/15/17";
+		date2 = "10/21/17";
+		a = false;;
 	}
 
 	public String getDates() 
@@ -27,35 +33,58 @@ public class ChatbotDevin implements Topic {
 		Date date = new Date();
 		return date.toString();
 	}
-	public String getCal()
+	public String getCal(int x)
 	{
 		Calendar cal = Calendar.getInstance();
-		cal.set(2017, 9, 16);//0 for Jan, 1 for Feb, etc...
-		cal.add(Calendar.MONTH, 2); // You can -/+ x months here to go back in history or move forward.
-		SimpleDateFormat dt = new SimpleDateFormat("MM/dd/yy"); 
-		long onedayago = cal.getTime().getTime() - 24 * 60 * 60 * 1000;
+		cal.set(2017, 9, 11);//0 for Jan, 1 for Feb, etc...
+		//cal.add(Calendar.MONTH, 2); // You can -/+ x months here to go back in history or move forward.
+		SimpleDateFormat dt = new SimpleDateFormat("dd"); 
+		long onedayago = cal.getTime().getTime() + x * 24 * 60 * 60 * 1000 - cal.getTime().getTime();
 		cal.setTime(new Date(onedayago));
 		//System.out.println(dt.format(cal.getTime()));
 		return dt.format(cal.getTime()); // New date
+		
 	}
 	@Override
 	public void talk(String response) {
 		ChatbotMain.print("Events? What events do you want to know?");
 		response = ChatbotMain.getInput();
+	
+		for (int i = 0; i < facts.length; i++) {
+			if(ChatbotMain.findKeyword(response,facts[i] , 0)>=0) {
+			ChatbotMain.print("Some events that are happening around this area is: L.E.S Pickle Day at "+date1+" and Giant Pumpkin Weekend at "+date2+". "
+					+ "L.E.S Pickle Day is "+getCal(5)+" days away from today while Giant Pumpkin Weekend is "+getCal(11)+" days away.");//hard-coded
+			
+			response = ChatbotMain.getInput();
+
+			
+				
+
 		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
+			
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
-				ChatbotMain.print("Here's the date: "+getDates()+" ");
+				ChatbotMain.print("Here's the date and time for today: "+getDates()+" ");
 				response = ChatbotMain.getInput();
 				
-			}else {
-				ChatbotMain.print(""+getCal()+"");
+			}
+
+			else {
+				
+				ChatbotMain.print("Is that so? Sorry, but you should probably say something else!");
 				response = ChatbotMain.getInput();
+				
+				
+				
 			}
 		}
 		//access variables from other classes
-		ChatbotMain.print("Well, it was nice talking to you, "+ChatbotMain.chatbot.getUsername()+"!");
+		ChatbotMain.print("Well, it was nice talking to you"+ChatbotMain.chatbot.getUsername()+"!");
 		ChatbotMain.chatbot.startChatting();
+			}
+		}
 	}
+	
+	
 	@Override
 	public boolean isTriggered(String response) {
 		for(int i = 0; i < keywords.length; i++)
