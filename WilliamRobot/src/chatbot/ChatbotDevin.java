23ad8193f25de6008667008a2363c0d070111d95
yingz <yingz@LAPTOP-APEKNB6S.fios-router.home>
2017-10-12 21:24:24 -0400

    i can't anymore

--- WilliamRobot/src/chatbot/ChatbotDevin.java
+++ WilliamRobot/src/chatbot/ChatbotDevin.java
@@ -11,7 +11,9 @@ public class ChatbotDevin implements Topic {
 	private String[] facts;
 	private String date1;
 	private String date2;
-	private boolean a;
+	private String[] angry;
+	private boolean stop;
+	private int counter;
 	
 	
 	//
@@ -22,10 +24,16 @@ public class ChatbotDevin implements Topic {
 		goodbyeKeyword = "bye";
 		secretKeyword = "time";
 		String[] knowledge = {"general","area","recommend"};
+		
 		facts = knowledge;
 		date1 = "10/15/17";
 		date2 = "10/21/17";
-		a = false;;
+		
+		String[] mad = {"Is that so? Sorry, but you should probably say something else.", "Hey, you're getting on my nerves, can you stop?", "STOP, PLEASE.", "*sigh*"};
+		angry = mad;
+		stop = false;
+		counter = 0;
+		
 	}
 
 	public String getDates() 
@@ -47,42 +55,64 @@ public class ChatbotDevin implements Topic {
 	}
 	@Override
 	public void talk(String response) {
-		ChatbotMain.print("Events? What events do you want to know?");
+		ChatbotMain.print("Events? What kind of events do you want to know?");
 		response = ChatbotMain.getInput();
 	
 		for (int i = 0; i < facts.length; i++) {
-			if(ChatbotMain.findKeyword(response,facts[i] , 0)>=0) {
+		if(ChatbotMain.findKeyword(response,facts[i] , 0)>=0) {
+	
 			ChatbotMain.print("Some events that are happening around this area is: L.E.S Pickle Day at "+date1+" and Giant Pumpkin Weekend at "+date2+". "
 					+ "L.E.S Pickle Day is "+getCal(5)+" days away from today while Giant Pumpkin Weekend is "+getCal(11)+" days away.");//hard-coded
 			
 			response = ChatbotMain.getInput();
-
+			
+	}
+			
+			
 			
 				
 
-		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) {
-			
+		while(ChatbotMain.findKeyword(response, goodbyeKeyword, 0) == -1) 
 			if(ChatbotMain.findKeyword(response, secretKeyword, 0) >= 0) {
 				ChatbotMain.print("Here's the date and time for today: "+getDates()+" ");
 				response = ChatbotMain.getInput();
-				
 			}
 
-			else {
-				
-				ChatbotMain.print("Is that so? Sorry, but you should probably say something else!");
-				response = ChatbotMain.getInput();
-				
-				
-				
-			}
-		}
+				else {
+					if(!stop && counter < 4) {
+					ChatbotMain.print(angry[counter]);
+					response = ChatbotMain.getInput();
+					
+					}
+				if (counter > 2) {
+					ChatbotMain.print("alright im done with you, " +ChatbotMain.chatbot.getUsername()+". Bye.");
+					ChatbotMain.chatbot.startChatting();
+				}
+				else {
+					counter ++;
+					
+				}
+
+				}
+
 		//access variables from other classes
-		ChatbotMain.print("Well, it was nice talking to you"+ChatbotMain.chatbot.getUsername()+"!");
+		ChatbotMain.print("Well, it was nice talking to you "+ChatbotMain.chatbot.getUsername()+"!");
 		ChatbotMain.chatbot.startChatting();
-			}
+				}
 		}
-	}
+		
+		//access variables from other classes
+		//ChatbotMain.print("Well, it was nice talking to you "+ChatbotMain.chatbot.getUsername()+"!");
+		//ChatbotMain.chatbot.startChatting();
+		
+
+		
+
+		
+			
+			
+		
+	
 	
 	
 	@Override
